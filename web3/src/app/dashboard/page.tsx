3eaaffa4f384623996f858c55ee36eef7cacab4d
AdamNajajreh <adamnajajreh@gmail.com>
2025-05-23 17:38:11 +0200

    New structure

---
+++ web3/src/app/dashboard/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import GlassOverlay from '@/components/Dashboard/GlassOverlay'
+import GraphScene from '@/components/Dashboard/GraphScene'
+import DashboardComp from '@/components/Dashboard/DashboardComp'
+
+export default function Dashboard() {
+  return (
+    <div className="relative w-full h-screen overflow-hidden bg-gray-900">
+      {/* Background Graph */}
+      <div className="absolute inset-0 z-0">
+        <GraphScene />
+      </div>
+
+      {/* Blur overlay */}
+      <div className="absolute inset-0 z-10">
+        <GlassOverlay />
+      </div>
+
+      {/* UI on top */}
+      <div className="relative z-20 px-6 py-10">
+        <DashboardComp />
+      </div>
+    </div>
+  )
+}
---
+++ web3/src/components/Dashboard/GlassOverlay.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+export default function GlassOverlay() {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        backdropFilter: "blur(3px)",
+        background: "rgba(255, 255, 255, 0.05)",
+        zIndex: 1,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#ffffff",
+        fontSize: "2rem",
+      }}
+    >
+      Your UI Here
+    </div>
+  );
+}
