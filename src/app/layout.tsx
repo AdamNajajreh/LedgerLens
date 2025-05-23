ee7f2f3cfedcf1b0903cca1f6d707256aeb8f078
AdamNajajreh <adamnajajreh@gmail.com>
2025-05-23 12:29:01 +0200

    Empty pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -24,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
